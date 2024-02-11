41deb007dd32e56e37c90c50038456f2fcda9cba
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-02-11 19:14:58 +0200

    Update basic_controller.py

--- pymarl-master/src/controllers/basic_controller.py
+++ pymarl-master/src/controllers/basic_controller.py
@@ -66,8 +66,8 @@ class BasicMAC:
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
-        agent_outputs,_, _, _ = self.forward(ep_batch, t_ep, test_mode=test_mode)
-        chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
+        agent_outputs, out_estimate, mean, var = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode,out_estimate)
         return chosen_actions
 
     def forward(self, ep_batch, t, test_mode=False):
