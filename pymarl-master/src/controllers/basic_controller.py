f0c35ebc2dcbff3f0becb4e489348cfadd5c7892
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-02-11 18:49:22 +0200

    Update basic_controller.py

--- pymarl-master/src/controllers/basic_controller.py
+++ pymarl-master/src/controllers/basic_controller.py
@@ -1,6 +1,53 @@
 from modules.agents import REGISTRY as agent_REGISTRY
 from components.action_selectors import REGISTRY as action_REGISTRY
-import torch as th
+import torch as the
+import torch.nn as nn
+
+class VAE(nn.Module):
+
+    def __init__(self, input_dim=12, hidden_dim=200, latent_dim=100, prob_dim = 3):
+        super(VAE, self).__init__()
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, latent_dim),
+            nn.LeakyReLU(0.2)
+            )
+        
+        # latent mean and variance 
+        self.mean_layer = nn.Linear(latent_dim, 2)
+        self.logvar_layer = nn.Linear(latent_dim, 2)
+        
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(2, latent_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(latent_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+            )
+     
+    def encode(self, x):
+        x = self.encoder(x)
+        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
+        return mean, logvar
+
+    def reparameterization(self, mean, var):
+        epsilon = th.randn_like(var)  
+        z = mean + var*epsilon
+        return z
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        mean, logvar = self.encode(x)
+        z = self.reparameterization(mean, logvar)
+        x_hat = self.decode(z)
+        return x_hat, mean, log_var
 
 
 # This multi-agent controller shares parameters between agents
